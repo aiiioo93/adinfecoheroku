2f6b4ee9cbd85e78dc38bd136397d79c0cbd2d03
aiiioo93 <smbbdrame@gmail.com>
2023-05-04 20:13:15 +0200

    PASSAGE DE GET A POST

--- gestion/views.py
+++ gestion/views.py
@@ -17,52 +17,52 @@ def index(request):
 
 
 def appartement_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         appartements = Appartement.objects.all()
         return render(request, 'appartement_list.html', {'appartements': appartements} )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 def locataire_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         locataires = Locataire.objects.all()
         return render(request, 'locataire_list.html', {'locataires': locataires}
                       )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 def affectation_appartement_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         affectation_appartements = AffectationAppartement.objects.all()
         return render(request, 'affectation_appartement_list.html', {'affectation_appartements': affectation_appartements} )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 def etat_des_lieux_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         etat_des_lieux = EtatDesLieux.objects.all()
         return render(request, 'etat_des_lieux_list.html', {'etat_des_lieux': etat_des_lieux} )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 def paiement_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         paiements = Paiement.objects.all()
         return render(request, 'paiement_list.html', {'paiements': paiements} )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 def quittance_loyer_list(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
         quittance_loyers = QuittanceLoyer.objects.all()
         return render(request, 'quittance_loyer_list.html', {'quittance_loyers': quittance_loyers} )
     else:
-        return HttpResponseNotAllowed(['GET'])
+        return HttpResponseNotAllowed(['POST'])
 
 
 
