60d83fdcde3f8c7d1e2cce3e80f98bf1f9a592a0
aiiioo93 <smbbdrame@gmail.com>
2023-05-04 04:20:46 +0200

    Test en tdd de la classe Appartement avec l'ajout du contenu de classe

--- gestion/models.py
+++ gestion/models.py
@@ -10,5 +10,15 @@ class Locataire(models.Model):
         return self.nom
 
 class Appartement(models.Model):
-    pass
+        adresse = models.CharField(max_length=200)
+        complement_adresse = models.CharField(max_length=100)
+        ville = models.CharField(max_length=50)
+        code_postal = models.CharField(max_length=10)
+        prix_charges = models.DecimalField(max_digits=10, decimal_places=2)
+        loyer = models.DecimalField(max_digits=10, decimal_places=2)
+        depot_garantie = models.DecimalField(max_digits=10, decimal_places=2)
+        locataire = models.ForeignKey(Locataire, on_delete=models.SET_NULL, null=True, blank=True)
+
+        def __str__(self):
+            return self.adresse
 
