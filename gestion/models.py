ca4ebd0c4f2a3d50c83c3c35cc65bd11d91f55cb
aiiioo93 <smbbdrame@gmail.com>
2023-05-04 04:10:26 +0200

    Test en tdd de la classe Locataire avec l'ajout du contenu de classe

--- gestion/models.py
+++ gestion/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class Locataire(models.Model):
-    pass
+    nom = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=200)
+    numero_telephone = models.CharField(max_length=20)
+    adresse_email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.nom
 
