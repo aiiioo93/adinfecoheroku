0ad5496fbf5f14c50dfd59b03d55ecd8f2c3ddb7
aiiioo93 <smbbdrame@gmail.com>
2023-05-04 04:08:29 +0200

    Test en tdd de la classe Locataire

--- gestion/tests.py
+++ gestion/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Locataire
 
-# Create your tests here.
+class LocataireTest(TestCase):
+    def setUp(self):
+        self.locataire = Locataire.objects.create(nom="Dupont", adresse="1 rue de la Paix", numero_telephone="0123456789", adresse_email="dupont@example.com")
+
+    def test_str(self):
+        self.assertEqual(str(self.locataire), "Dupont")
